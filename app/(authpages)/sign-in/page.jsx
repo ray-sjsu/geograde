c7eea14dc6de4fa025dd84e944a00c8ee7af2680
ray-sjsu <raymund.mercader@sjsu.edu>
2024-09-20 21:41:08 -0700

    Working sign-in with email and password

---
+++ app/(authpages)/sign-in/page.jsx
@@ -0,0 +1,66 @@
+"use client";
+import { useState } from "react";
+import {
+  useSignInWithEmailAndPassword,
+  useAuthState,
+} from "react-firebase-hooks/auth";
+import { auth } from "@/app/firebase/config";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
+  const [user, loading, error] = useAuthState(auth);
+  const router = useRouter();
+
+  const handleSignIn = async () => {
+    try {
+      const res = await signInWithEmailAndPassword(email, password);
+      console.log({ res });
+      if (user) {
+        setEmail("");
+        setPassword("");
+        router.push("/");
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 flex-col space-y-2">
+      <div className="bg-gray-800 p-10 rounded-lg shadow-xl w-96">
+        <h1 className="text-white text-2xl mb-5">Sign In</h1>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
+        />
+        <button
+          onClick={handleSignIn}
+          className="w-full p-3 bg-indigo-600 rounded text-white hover:bg-indigo-500"
+        >
+          Sign In
+        </button>
+      </div>
+      <div className="bg-gray-800 p-10 rounded-lg shadow-xl w-96">
+        <Link href="/sign-up" className="p-2 bg-purple-400 text-xl rounded">
+          Click here to create an account
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ app/(authpages)/sign-up/page.jsx
@@ -0,0 +1,62 @@
+"use client";
+import { useState } from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "@/app/firebase/config";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [createUserWithEmailAndPassword] =
+    useCreateUserWithEmailAndPassword(auth);
+  const router = useRouter();
+
+  const handleSignUp = async () => {
+    try {
+      const res = await createUserWithEmailAndPassword(email, password);
+      console.log({ res });
+      sessionStorage.setItem("user", true);
+      setEmail("");
+      setPassword("");
+      router.push("/sign-in");
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 flex-col space-y-2">
+      <div className="bg-gray-800 p-10 rounded-lg shadow-xl w-96">
+        <h1 className="text-white text-2xl mb-5">Sign Up</h1>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full p-3 mb-4 bg-gray-700 rounded outline-none text-white placeholder-gray-500"
+        />
+        <button
+          onClick={handleSignUp}
+          className="w-full p-3 bg-indigo-600 rounded text-white hover:bg-indigo-500"
+        >
+          Sign Up
+        </button>
+      </div>
+      <div className="bg-gray-800 p-10 rounded-lg shadow-xl w-96">
+        <Link href="/sign-in" className="p-2 bg-purple-400 text-xl rounded">
+          Click here if you have an account
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
