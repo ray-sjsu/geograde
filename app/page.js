03fb5a54218f8ef60ce26f1a7533fa18cb72dca7
ray-sjsu <raymund.mercader@sjsu.edu>
2024-10-04 15:20:36 -0700

    Remove email/password as auth option

--- app/page.js
+++ app/page.js
@@ -3,13 +3,11 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/app/firebase/config";
 import { useRouter } from "next/navigation";
 import { signOut } from "firebase/auth";
+import Link from "next/link";
 
 export default function Home() {
   const [user] = useAuthState(auth);
   const router = useRouter();
-  if (!user) {
-    router.push("/sign-in");
-  }
 
   return (
     <div className="flex items-center justify-center flex-col min-h-screen bg-gray-100">
@@ -17,13 +15,24 @@ export default function Home() {
         <h1 className="text-2xl font-bold text-gray-800 mb-2">
           Welcome to the Landing Page
         </h1>
-        <p className="text-gray-600 mb-4">You are signed in</p>
-        <button
-          onClick={() => signOut(auth)}
-          className="px-4 py-2 bg-red-500 text-white font-semibold rounded-md hover:bg-red-600 transition-colors duration-300"
-        >
-          Log Out
-        </button>
+        <p className="text-gray-600 mb-4">
+          {user ? "You are signed in" : "You are signed out"}
+        </p>
+        {user ? (
+          <button
+            onClick={() => signOut(auth)}
+            className="px-4 py-2 bg-red-500 text-white font-semibold rounded-md hover:bg-red-600 transition-colors duration-300"
+          >
+            Log Out
+          </button>
+        ) : (
+          <Link
+            href="sign-in"
+            className="px-4 py-2 bg-green-500 text-white font-semibold rounded-md hover:bg-red-600 transition-colors duration-300"
+          >
+            Sign In
+          </Link>
+        )}
       </div>
     </div>
   );
