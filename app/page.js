e2ef58f26a9364c57c12694d49ff74aeb200bf94
ray-sjsu <raymund.mercader@sjsu.edu>
2024-10-18 22:31:23 -0700

    Working version of Google Firebase Auth + Prisma Account Creation

--- app/page.js
+++ app/page.js
@@ -1,13 +1,6 @@
-"use client";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "@/app/firebase/config";
-import { useRouter } from "next/navigation";
-import { signOut } from "firebase/auth";
-import Link from "next/link";
+import SignInOutSection from "@/components/auth-components/SignInOutButton";
 
 export default function Home() {
-  const [user] = useAuthState(auth);
-  const router = useRouter();
 
   return (
     <div className="flex items-center justify-center flex-col min-h-screen bg-gray-100">
@@ -15,24 +8,7 @@ export default function Home() {
         <h1 className="text-2xl font-bold text-gray-800 mb-2">
           Welcome to the Landing Page
         </h1>
-        <p className="text-gray-600 mb-4">
-          {user ? "You are signed in" : "You are signed out"}
-        </p>
-        {user ? (
-          <button
-            onClick={() => signOut(auth)}
-            className="px-4 py-2 bg-red-500 text-white font-semibold rounded-md hover:bg-red-600 transition-colors duration-300"
-          >
-            Log Out
-          </button>
-        ) : (
-          <Link
-            href="sign-in"
-            className="px-4 py-2 bg-green-500 text-white font-semibold rounded-md hover:bg-red-600 transition-colors duration-300"
-          >
-            Sign In
-          </Link>
-        )}
+        <SignInOutSection />
       </div>
     </div>
   );
