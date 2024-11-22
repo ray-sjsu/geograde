084f90f436f9ecd87f16b6322225827e8a08ebcf
ray-sjsu <raymund.mercader@sjsu.edu>
2024-11-21 19:13:59 -0800

    Remove old code in favor of firestore.

--- prisma/migrations/20241106011455_review_image_support/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Image" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "url" TEXT NOT NULL,
-    "alt" TEXT,
-    "reviewId" TEXT NOT NULL,
-    CONSTRAINT "Image_reviewId_fkey" FOREIGN KEY ("reviewId") REFERENCES "Review" ("id") ON DELETE CASCADE ON UPDATE CASCADE
-);
